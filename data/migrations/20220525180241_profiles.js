b3bacfcda8592a604876875305efb6cd0cfe27b9
ashtilawat23 <ashtilawat23@gmail.com>
2022-05-25 14:19:51 -0400

    fixed linting

--- data/migrations/20220525180241_profiles.js
+++ data/migrations/20220525180241_profiles.js
@@ -2,22 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
-  .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-  .createTable('profiles', function(table) {
-    table.string('id').primary();
-    table.string('email');
-    table.string('role');
-    table.string('name');
-    table.timestamps(true, true);
-  })
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('profiles', function (table) {
+      table.string('id').primary();
+      table.string('email');
+      table.string('role');
+      table.string('name');
+      table.timestamps(true, true);
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('profiles');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('profiles');
 };
--- data/migrations/20220525181322_jobs.js
+++ data/migrations/20220525181322_jobs.js
@@ -2,22 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up = function(knex) {
-    return knex.schema.createTable('jobs', (table) => {
-        table.increments('id').primary();
-        table.string('title');
-        table.string('company');
-        table.string('description');
-        table.string('redirectUrl').notNullable();
-        table.string('approvedBy');
-        table.timestamps(true, true);
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable('jobs', (table) => {
+    table.increments('id').primary();
+    table.string('title');
+    table.string('company');
+    table.string('description');
+    table.string('redirectUrl').notNullable();
+    table.string('approvedBy');
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('jobs');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('jobs');
 };
